58da0e2c476d703c533561a83ceae4d2d6714fcc
Sanal Kumar <sanat@cisco.com>
2017-09-20 15:58:55 -0700

    impl async http

--- NYTimesSearch/app/src/main/java/com/codepath/week1/nytimessearch/SearchActivity.java
+++ NYTimesSearch/app/src/main/java/com/codepath/week1/nytimessearch/SearchActivity.java
@@ -1,10 +1,9 @@
 package com.codepath.week1.nytimessearch;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
     EditText etQuery;
@@ -57,6 +65,23 @@ public class SearchActivity extends AppCompatActivity {
 
     public void onArticleSearch(View view) {
         String query = etQuery.getText().toString();
-        Toast.makeText(this,"Searching for " + query,Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this,"Searching for " + query,Toast.LENGTH_SHORT).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        RequestParams params = new RequestParams();
+        params.put("api-key","fbe95cf398c3486bba648665321509be");
+        params.put("page",0);
+        params.put("q",query);
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("debug",response.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d("debug",responseString);
+            }
+        });
     }
 }
