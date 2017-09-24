2f0ec129b064630285c66e55c75b163403f289d2
Sanal Kumar <sanat@cisco.com>
2017-09-24 10:16:21 -0700

    impl Share the article

--- NYTimesSearch/app/src/main/java/com/codepath/week1/nytimessearch/activities/ArticleActivity.java
+++ NYTimesSearch/app/src/main/java/com/codepath/week1/nytimessearch/activities/ArticleActivity.java
@@ -1,8 +1,12 @@
 package com.codepath.week1.nytimessearch.activities;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -11,12 +15,14 @@ import com.codepath.week1.nytimessearch.R;
 import com.codepath.week1.nytimessearch.model.Article;
 
 public class ArticleActivity extends AppCompatActivity {
+    private ShareActionProvider miShareAction;
+    private Intent shareIntent;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
-
         final Article article = (Article) getIntent().getSerializableExtra("article");
 
         WebView webView = (WebView)findViewById(R.id.wvArticle);
@@ -27,6 +33,41 @@ public class ArticleActivity extends AppCompatActivity {
                 return true;
             }
         });
-        webView.loadUrl(article.getLink());
+        url = article.getLink();
+        attachShareIntent(url);
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.article_menu, menu);
+        MenuItem item = menu.findItem(R.id.action_share);
+        // Fetch reference to the share action provider
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        attachShareIntentAction();
+        // Return true to display menu
+        return true;
+    }
+
+    public void attachShareIntentAction() {
+        if (miShareAction != null && shareIntent != null)
+            miShareAction.setShareIntent(shareIntent);
+    }
+
+    public void attachShareIntent(String url) {
+        shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, url);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_share) {
+            startActivity(Intent.createChooser(shareIntent, "Share link using"));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
